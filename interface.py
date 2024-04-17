1ef21e01f124bc2f234d6967b397448d23494e49
Othman EL HADRATI <othmandone@gmail.com>
2024-04-18 00:14:19 +0100

    first

--- interface.py
+++ interface.py
@@ -1,19 +1,25 @@
 import tkinter as tk
+import customtkinter as ctk
 from PIL import Image, ImageTk
 import app
-top = tk.Tk()
-top.title("Weather App")
-top.state('zoomed')
-top.resizable(height=False, width=False)
-app.backgroundApp(top)
+root = tk.Tk()
+root.title("Weather App")
+root.state('zoomed')
+root.resizable(height=False, width=False)
+app.backgroundApp(root)
 
-mainFrame = tk.Frame(top)
+mainFrame = tk.Frame(root , background="#ffffff" , width=1320 , height=730)
+mainFrame.place(x=115 , y=50)
+
+#search bar
+searchBar = ctk.CTkEntry(mainFrame ,width=300 , height=40 , placeholder_text="Location" , font=('Poppins Medium' ,17 ) )
+searchBar.place(x=1000, y=30)
 
 
 
 
 
-top.mainloop()
+root.mainloop()
 
 
 
