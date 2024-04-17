3015578d91813ef311a8a1fd2650510cbd62731f
Othman EL HADRATI <othmandone@gmail.com>
2024-04-17 17:50:14 +0100

    first

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-import requests
+import tkinter as tk
+import ipywidgets
+import seaborn
+import matplotlib
+import geopy
+import folium
+import customtkinter as ctk
+from  PIL import ImageTk
+
+app = tk.Tk()
+app.title("Weather App")
+app
 
 
 
-# Base URL for the 7Timer API (astro endpoint)
-url = f"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
 
-# Make a request to the 7Timer API
-response = requests.get(url)
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON data from the response
-    data = response.json()
-    print(data)
+
+
+
+
+
+
+
+
 
 
