627740dea094cbc4a1c9d9c56c19aa8fbac5a3b5
Othman EL HADRATI <othmandone@gmail.com>
2024-04-17 18:17:17 +0100

    first

--- app.py
+++ app.py
@@ -9,40 +9,23 @@ from PIL import Image, ImageTk
 import tkintermapview
 
 
-def load_background_image(canvas, image_path):
-    # Ouvrir l'image à partir du chemin
-    image = Image.open(image_path)
+top = tk.Tk()
+top.state('zoomed')  # Maximisez la fenêtre
 
-    # Redimensionner l'image pour correspondre à la taille du canevas
-    canvas_width = canvas.winfo_width()
-    canvas_height = canvas.winfo_height()
+# Chargez l'image d'arrière-plan
+image_path = ".\\static\\landscape-forest-trees-mist-wallpaper-preview.jpg"  # Remplacez par votre chemin d'image
+image = Image.open(image_path)
 
-    # Utilisez Image.Resampling.LANCZOS pour redimensionner l'image avec une bonne qualité
-    image = image.resize((canvas_width, canvas_height), Image.Resampling.LANCZOS)
+# Redimensionnez l'image pour remplir la fenêtre
+window_width = top.winfo_screenwidth()
+window_height = top.winfo_screenheight()
+image = image.resize((window_width, window_height), Image.Resampling.LANCZOS)
 
-    # Convertir l'image PIL en une image que tkinter peut utiliser
-    background_image = ImageTk.PhotoImage(image)
+# Convertissez l'image PIL en PhotoImage utilisable par tkinter
+background_image = ImageTk.PhotoImage(image)
 
-    # Afficher l'image comme arrière-plan dans le canevas
-    canvas.create_image(0, 0, anchor=tk.NW, image=background_image)
+# Créez un Label avec l'image d'arrière-plan
+background_label = tk.Label(top, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    # Stocker la référence à l'image pour éviter qu'elle ne soit collectée par le garbage collector
-    canvas.background_image = background_image
-
-
-# Créez la fenêtre principale
-root = tk.Tk()
-
-# Maximisez la fenêtre
-root.state('zoomed')
-
-# Créez un canevas et placez-le dans la fenêtre
-canvas = tk.Canvas(root)
-canvas.pack(fill=tk.BOTH, expand=True)
-
-# Chemin de l'image d'arrière-plan
-image_path = ".\\static\\landscape-forest-trees-mist-wallpaper-preview.jpg"  # Remplacez par le chemin de votre image
-
-load_background_image(canvas, image_path)
-
-root.mainloop()
+top.mainloop()
