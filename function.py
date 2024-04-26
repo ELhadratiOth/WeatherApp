a67c59c1924bb6e0b3ab9bbe1abdf38884322ebd
Othman EL HADRATI <othmandone@gmail.com>
2024-04-26 14:20:12 +0100

    firsth

--- function.py
+++ function.py
@@ -2,6 +2,7 @@ import json
 from urllib.request import urlopen
 import requests
 import time
+import random
 
 # def actuall_data():
 #     url="http://ipinfo.io/json"
@@ -13,20 +14,21 @@ import time
 #     return latitude , longitude
 
 def getCurrentLocation(latitude, longitude):
-    weatherApiUrl = f"https://api.open-meteo.com/v1/dwd-icon?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,weather_code&forecast_days=1"
+    weatherApiUrl = f"https://api.open-meteo.com/v1/dwd-icon?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,weather_code&forecast_hours=24"
     getting_weather= requests.get(weatherApiUrl)
     currentWeather = getting_weather.json()
     print(currentWeather)
     print('getCurrentLocation')
-
     return currentWeather
 
 
 def oganiseData(latitude, longitude):
     currentWeather = getCurrentLocation(latitude, longitude)
-    time = currentWeather['hourly']['time'][::3]
-    tmp = currentWeather['hourly']['temperature_2m'][::3]
-    tmp_code = currentWeather['hourly']['weather_code'][::3]
+    indices_utilises = random.sample(range(24), 6)
+    indices_utilises.sort()
+    time = [ currentWeather['hourly']['time'][i] for i in indices_utilises ]
+    tmp = [currentWeather['hourly']['temperature_2m'][i] for i in indices_utilises ]
+    tmp_code = [currentWeather['hourly']['weather_code'][i] for i in indices_utilises ]
     print(tmp_code)
     dataDic = {
         'time': time ,
@@ -75,14 +77,19 @@ def actuall_dataCompli():
     return latitude , longitude , region , city
 def getCurrentWeather(latitude, longitude):
     weatherApiUrl = f"https://api.open-meteo.com/v1/dwd-icon?latitude={latitude}&longitude={longitude}&current=temperature_2m,weather_code&forecast_days=1"
+
     getting_weather= requests.get(weatherApiUrl)
-    currentWeather = getting_weather.json()
-    print(currentWeather)
-    lastT = currentWeather['current']['temperature_2m']
-    code_lastT = currentWeather['current']['weather_code']
+    if getting_weather.status_code == 200:
+        currentWeather = getting_weather.json()
+        print(currentWeather)
+        lastT = currentWeather['current']['temperature_2m']
+        code_lastT = currentWeather['current']['weather_code']
 
 
-    return lastT , code_lastT
+        return lastT , code_lastT
+    else :
+            print("error de connexion")
+            return
 
 
 def outputCurrentT(latitude, longitude):
@@ -111,6 +118,39 @@ def outputCurrentT(latitude, longitude):
 
     return str(lastT).split('.')[0] , img
 
+def getAirQuality(aqi):
+    if aqi <= 20:
+        return "Good", "Enjoy outdoor activities!" , '#1F8A70'
+    elif aqi <= 50:
+        return "Fair", "Acceptable, though some may be sensitive." , '#219C90'
+    elif aqi <= 100:
+        return "Moderate", "Sensitive individuals may experience symptoms." , '#FFC700'
+    elif aqi <= 150:
+        return "Poor", "Effects possible for sensitive people." ,'#FF9800'
+    elif aqi <= 250:
+        return "Very Poor", "Health alert: serious effects possible." , "#DD5746"
+    else:
+        return "Extremely Poor", "Emergency conditions. High risk for everyone." , "#E72929"
+
+def AirDetails(latitude, longitude):
+    weatherApiUrl = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={latitude}&longitude={longitude}&current=european_aqi,us_aqi,pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,sulphur_dioxide,ozone&timezone=auto&forecast_days=1"
+    getting_weather= requests.get(weatherApiUrl)
+    currentWeather = getting_weather.json()
+    print(currentWeather)
+    aqi = int( (currentWeather['current']['european_aqi'] + currentWeather['current']['us_aqi']) / 2 )
+
+    pm10 = currentWeather['current']['pm10']
+    pm2_5 = currentWeather['current']['pm2_5']
+    carbon_monoxide = currentWeather['current']['carbon_monoxide']
+    nitrogen_dioxide = currentWeather['current']['nitrogen_dioxide']
+    sulphur_dioxide = currentWeather['current']['sulphur_dioxide']
+    ozone = currentWeather['current']['ozone']
+    time = currentWeather['current']['time'].split('T')[1]
+    unity = currentWeather['current_units']['pm10']
+
+
+    type , message , color = getAirQuality(aqi)
+    return aqi , pm10 , pm2_5 , carbon_monoxide , nitrogen_dioxide , sulphur_dioxide , ozone , type , message , time , unity , color
 
 # lastT , img = outputCurrentT()
 # print(lastT , img)
